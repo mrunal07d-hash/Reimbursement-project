3ee82c7ba904fdb4d8756706bb002d5e806090ef
mrunal07d-hash <mrunal07d@gmail.com>
2026-03-29 14:30:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,26 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import { connectDB } from "./backend/config/db.js";
 
-import authRoutes from "./routes/auth.js";
-import expenseRoutes from "./routes/expenses.js";
-import approvalRoutes from "./routes/approvals.js";
-import currencyRoutes from "./routes/currency.js"; // ✅ added
+import authRoutes from "./backend/routes/auth.js";
+import expenseRoutes from "./backend/routes/expenses.js";
+import approvalRoutes from "./backend/routes/approvals.js";
+import currencyRoutes from "./backend/routes/currency.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/approvals", approvalRoutes);
-app.use("/api/currency", currencyRoutes); // ✅ added BEFORE listen
+app.use("/api/currency", currencyRoutes);
 
-// Start server
 app.listen(5000, () => {
   console.log("Server running on 5000 🚀");
 });
